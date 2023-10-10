22ff2e30aca0c2dbbfae38c13754d138dbd88f56
Dani094 <117084576+SandyGarciaR@users.noreply.github.com>
2023-10-10 17:51:32 -0500

    arreglos hector

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,16 @@
       <div class="arriba"></div>
       <div class="recto">
         <div class="foto">
-
+          <select class="form-select" aria-label="Default select example" v-model="tipoSeleccionado" placeholder="tipos" @change="filtrarpokemon()" style="text-align: center;">
+            <option value="Todos">Todos</option>
+            <option v-for="(color, tipo) in colorestipo" :key="color" :value="tipo" >
+              {{ tipo }}
+            </option>
+          </select>
         </div>
         <div class="buscar">
-          <input type="text">
-          <button>BUSCAR</button>
+          <input type="text" v-model="nombrepokemon">
+          <button @click="filtrarPokemonPorNombre">BUSCAR</button>
         </div>
       </div>
       <div class="bola">
@@ -17,7 +22,19 @@
       <div class="abajo"></div>
     </div>
     <div class="cartas">    
-      <div v-for="(pokemon, index) in pokemons.slice(0, numeroDeCartas)" :key="index" class="card">
+      <div v-for="(pokemon, index) in filtropokemones" :key="index" class="card" v-if="activarfiltro == true && filtronombre == false">
+        <button  class="card-content" @click="abrirDetalle(index)">
+          <div class="card-number">#{{ pokemon.numero }}</div>
+          <div class="card-name">{{ pokemon.nombre }}</div>
+          <img :src="pokemon.img" :alt="'Imagen de ' + pokemon.nombre" class="pokemon-image" />
+          <div style="display: flex; justify-content: center; height: 10%; gap: 10%;">
+            <div v-for="(tipo, i) in pokemon.tipos" :key="i" :style="'background-color: ' + colorestipo[tipo]" class="pokemon-type">
+              <p>{{ tipo }}</p>
+            </div>
+          </div>
+        </button>
+      </div>
+      <div v-for="(pokemon, index) in pokemons" :key="index" class="card" v-if="activarfiltro == false && filtronombre == false">
         <button  class="card-content" @click="abrirDetalle(index)">
           <div class="card-number">#{{ pokemon.numero }}</div>
           <div class="card-name">{{ pokemon.nombre }}</div>
@@ -30,6 +47,21 @@
         </button>
       </div>
     </div>
+
+    <div class="card" v-if="filtronombre == true">
+        <button  class="card-content" @click="abrirDetalle(index)">
+          <div class="card-number">#{{ pokemonBuscado.numero }}</div>
+          <div class="card-name">{{ pokemonBuscado.nombre }}</div>
+          <img :src="pokemonBuscado.img" :alt="'Imagen de ' + pokemonBuscado.nombre" class="pokemon-image" />
+          <div style="display: flex; justify-content: center; height: 10%; gap: 10%;">
+            <div v-for="(tipo, i) in pokemonBuscado.tipos" :key="i" :style="'background-color: ' + colorestipo[tipo]" class="pokemon-type">
+              <p>{{ tipo }}</p>
+            </div>
+          </div>
+        </button>
+      </div>
+
+    
 
   </div>
 
@@ -94,11 +126,19 @@ const colorestipo = {
   water: "#2196F3",
   bug: "#6B8E23",
   normal: "#808080",
-  electric: "#FFFF00",
+  electric: "#D7CD04",
   ground: "#8B4513",
-  fairy: "#FF69B4"
+  fairy: "#FF69B4",
+  ghost: "#4B0082",
+  steel: "#A9A9A9",
+  dark: "#2F4F4F",
+  dragon: "#483D8B",
+  rock: "#A0522D",
+  psychic: "#FF1493",
+  ice: "#00CED1"
 };
 const numeroDeCartas = ref(20); // Inicialmente, muestra 20 cartas
+const filtropokemones = ref([]);
 
 async function obtenerurlpokemon() {
   for (let i = 1; i <= 1200; i++) {
@@ -124,7 +164,11 @@ async function obtenerurlpokemon() {
 }
 
 function abrirDetalle(index) {
-  detallePokemon.value = pokemons.value[index];
+  console.log(index);
+  console.log(filtropokemones.value);
+  if(activarfiltro.value==true){
+    detallePokemon.value = filtropokemones.value[index]
+  }else detallePokemon.value = pokemons.value[index];
   mostrarModal.value = true;
 }
 
@@ -133,12 +177,46 @@ function cerrarDetalle() {
 }
 
 function mostrarMas() {
-  numeroDeCartas.value += 20; // Incrementa el número de cartas a mostrar
+  numeroDeCartas.value += 50; // Incrementa el número de cartas a mostrar
 }
 
 onMounted(() => {
   obtenerurlpokemon();
 });
+
+const pokemonBuscado = ref({});
+const nombrepokemon = ref("");
+const filtronombre = ref(false);
+
+function filtrarPokemonPorNombre() {
+  console.log(nombrepokemon.value);
+  pokemons.value.forEach((pokemon) => {
+    if (pokemon.nombre === nombrepokemon.value.toUpperCase()) {
+      pokemonBuscado.value = pokemon;
+      filtronombre.value = true;
+    }
+  });
+}
+
+
+const activarfiltro = ref(false);
+const tipoSeleccionado = ref("Todos")
+
+function filtrarpokemon() {
+  if (tipoSeleccionado.value == "Todos") {
+    activarfiltro.value = false;
+    return;
+  }
+
+  filtropokemones.value = [];
+  pokemons.value.forEach((pokemon) => {
+    if (pokemon.tipos.includes(tipoSeleccionado.value)) {
+      filtropokemones.value.push(pokemon);
+    }
+  });
+
+  activarfiltro.value = true;
+}
 
 </script>
 
@@ -337,5 +415,14 @@ button {
   background: rgb(0, 0, 0);
   font-size: 25px;
   color: rgb(255, 255, 255);
+}
+table{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+td{
+  padding: 10px;
 }
 </style>
